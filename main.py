b30b6cf3215ccc2e110fc3d33d0eaeec22d08c35
Geraint White <geraintwhite@gmail.com>
2017-02-16 21:20:52 +0000

    read binary from stdin

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ import sys
 
 
 if (len(sys.argv) < 2):
-    data = sys.stdin.read()
+    data = sys.stdin.buffer.read()
 else:
-    data = ' '.join(sys.argv[1:])
+    data = bytes(' '.join(sys.argv[1:]), 'utf-8')
 
 if data == '-h' or data == '--help':
     print('Usage: python main.py sometext')
 else:
-    out = hash.colour_hash(bytes(data, 'utf-8'))
+    out = hash.colour_hash(data)
     print('\n'.join(''.join(i*2 for i in row) for row in hash.chunks(out, 8)))
